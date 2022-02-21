9c08c4027fe65156f0468a60361548af2d8e8d65
Dreanaught <dreanaught@gmx.net>
2022-02-21 12:34:40 +0000

    Adding a tiny bit of flesh to the parser

--- pyjucontrol/parsers/__init__.py
+++ pyjucontrol/parsers/__init__.py
@@ -5,5 +5,20 @@ class Parser:
 
     def parse_get_device_data(self, json):
         """parse the get device data message"""
-        for firstlevel in json:
-            print(firstlevel)
+        if json["status"].upper() != "OK":
+            return None
+        data = json["data"]
+        for listitem in data:
+            self.parse_device_data(listitem)
+        print("status:"+json["status"])
+
+    def parse_device_data(self, json):
+        """parse the device data"""
+        for item in json.items():
+            key = item[0]
+            value = item[1]
+            if(type(value) is str):
+                print(key+":"+value)
+            else:
+                print(key+":"+type(value).__name__)
+            
